5842aa55cdcbb7b4766f35a2b92c4d8dcf1bd5d6
mochabbang <sugnmin9036@gmail.com>
2023-09-14 09:20:59 +0900

    카테고리 클릭 시 api데이터 조회되도록 구성 - 23.09.14   - 배운거, env파일 관련해서는 import 파일 사용하지말고 그냥 process.env.변수명 으로 설정

--- apis/NewsApis.ts
+++ apis/NewsApis.ts
@@ -1,12 +1,15 @@
 import { NewsTopHeadLine } from '@/types/NewsTopHeadLine';
 import axios from 'axios';
-import { env } from 'process';
 
 // 탑 헤드라인 뉴스 목록 가져오기
-export const GetNewsTopHeadLines = async () => {
-    const apiKey = env.NEWS_API_KEY;
+export const GetNewsTopHeadLines = async (category: string) => {
+    //const key = apiKey === undefined ? env.NEXT_PUBLIC_NEWS_API_KEY : apiKey;
+    const key = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+    const query =
+        category === 'all' || category === '' ? '' : `&category=${category}`;
+
     const apiNewsTopHeadLines = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`,
+        `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${key}`,
     );
 
     const newsTopHeadLines: NewsTopHeadLine = apiNewsTopHeadLines.data;
