0de666e70e04012a95007dfadf5036bea948e8f5
이성민 <sungmin9036@gmail.com>
2023-09-26 09:03:35 +0900

    더이상없나.. 오늘도 css.. -23.09.26

--- components/NewsItem.tsx
+++ components/NewsItem.tsx
@@ -3,37 +3,39 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const NewsItem = (article: Article) => {
-    const { title, description, url, urlToImage } = article;
+    const { title, description, url, urlToImage, publishedAt } = article;
 
     return (
-        <div className="flex my-2 md:w-fit bg-white rounded-[4px]">
+        <div className="flex my-2 bg-white rounded-[4px] w-full">
             {urlToImage && (
                 <Link href={url} target="_blank" rel="noopener noreferrer">
-                    <div className="m-1 relative w-[130px] h-[100px] md:w-[100px] h-[70px]">
+                    <div className="m-1 relative w-[130px] h-[100px] md:min-w-[100px] h-[70px]">
                         <Image
                             src={urlToImage}
                             alt="thumname"
                             unoptimized
                             fill
                             style={{ objectFit: 'fill' }}
-                            sizes="(max-width:768px) 5vw"
                             className="rounded-[4px]"
                         />
                     </div>
                 </Link>
             )}
-            <div className="m-2 md:max-w-[300px] text-xs/[13px] font-semibold">
-                <Link
-                    href={url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-black"
-                >
-                    {title}
-                </Link>
+            <div className="m-2 md:max-w-[300px] text-sm/[13px] md:text-xs/[13px]">
+                <div className="font-semibold">
+                    <Link
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-black"
+                    >
+                        {title}
+                    </Link>
+                </div>
                 <p className="m-0 leading-none mt-2 text-gray-500 leading-4">
                     {description && description.substring(0, 100) + '...'}
                 </p>
+                <div className="mt-2 text-xs">게시일 : {publishedAt}</div>
             </div>
         </div>
     );
